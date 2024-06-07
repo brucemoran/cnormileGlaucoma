a2069f20c8050633929fa0231720d99ea6fec052
brucemoran <bruce01campus@gmail.com>
2024-06-07 15:31:22 +0100

    Update analyses

--- R/caoimhe_xlsx.R
+++ R/caoimhe_xlsx.R
@@ -1,29 +1,69 @@
 #' Run fgsea analysis on XLSX
 #' @param XLSX character 'all' or 'equal' to specify which data to load
 #' @param TAG character to tag output and name plots etc
+#' @param OUTDIR character where output is saved
 #' @rdname run_analysis
 #' @export
 
-run_analysis <- function(XLSX, TAG){
+run_analysis <- function(XLSX, TAG, OUTDIR){
+  print(paste0(" - ", TAG))
   data_list <- read_xlsx_glaucoma(XLSX)
   msigdb_pathlist <- msigdb_pathways_to_list()
   tb_stats <- data_list$tb$avg_log2FC
-  names(tb_stats) <- data_list$tb$X1
+  colnames(data_list$tb)[1] <- "gene"
+  names(tb_stats) <- data_list$tb$gene
   fgseaRes <- fgsea::fgsea(pathways = msigdb_pathlist,
                            stats    = tb_stats,
                            minSize  = 15,
                            maxSize  = 500)
   fgseaResTidy <- dplyr::arrange(.data = tibble::as_tibble(fgseaRes),
                                  desc(NES))
-  msigdb_pway_xlsx(fgseaRes, data_list$tb, padj = 0.05, TAG)
+  msigdb_pway_xlsx(fgseaRes, data_list$tb, padj = 0.05, TAG, OUTDIR)
   ggplot2::ggplot(fgseaResTidy, ggplot2::aes(reorder(pathway, NES), NES)) +
     ggplot2::geom_col(ggplot2::aes(fill=padj<0.05)) +
     ggplot2::coord_flip() +
     ggplot2::labs(x="Pathway", y="Normalized Enrichment Score",
          title=paste0(TAG, " - Hallmark pathways NES from GSEA")) +
     ggplot2::theme_minimal()
-  filenam <- paste0(TAG, ".", gsub(".xlsx", ".pathway.pdf", data_list$filename))
+  dir.create(paste0(OUTDIR, "/pdf/"), recursive = TRUE, showWarnings = FALSE)
+  filenam <- paste0(OUTDIR, "/pdf/", TAG, ".", gsub(".xlsx", ".pathway.pdf", data_list$filename))
   ggplot2::ggsave(filename = filenam)
+}
+
+#' Run fgsea analysis on grouped XLSX
+#' @param XLSX character 'all' or 'equal' to specify which data to load
+#' @param TAG character to tag output and name plots etc
+#' @param OUTDIR character where output is saved
+#' @rdname run_group_analysis
+#' @export
+
+run_group_analysis <- function(XLSX, TAG, OUTDIR){
+  print(paste0(" - ", TAG))
+  data_list <- read_xlsx_glaucoma(XLSX)
+  msigdb_pathlist <- msigdb_pathways_to_list()
+  ##split on cluster
+  res_list <- lapply(unique(unlist(data_list$tb$cluster)), function(ff){
+    TAG <- paste0(TAG, "_Cluster_", ff)
+    tb_ff <- dplyr::filter(.data = data_list$tb, cluster %in% ff)
+    tb_stats <- tb_ff$avg_log2FC
+    names(tb_stats) <- tb_ff$gene
+    fgseaRes <- fgsea::fgsea(pathways = msigdb_pathlist,
+                             stats    = tb_stats,
+                             minSize  = 15,
+                             maxSize  = 500)
+    fgseaResTidy <- dplyr::arrange(.data = tibble::as_tibble(fgseaRes),
+                                   desc(NES))
+    msigdb_pway_xlsx(fgseaRes, tb_ff, padj = 0.05, TAG, OUTDIR)
+    ggplot2::ggplot(fgseaResTidy, ggplot2::aes(reorder(pathway, NES), NES)) +
+      ggplot2::geom_col(ggplot2::aes(fill = padj<0.05)) +
+      ggplot2::coord_flip() +
+      ggplot2::labs(x="Pathway", y="Normalized Enrichment Score",
+                    title=paste0(TAG, " - Hallmark pathways NES from GSEA")) +
+      ggplot2::theme_minimal()
+    dir.create(paste0(OUTDIR, "/pdf/"), recursive = TRUE, showWarnings = FALSE)
+    filenam <- paste0(OUTDIR, "/pdf/", TAG, ".", gsub(".xlsx", ".pathway.pdf", data_list$filename))
+    ggplot2::ggsave(filename = filenam)
+  })
 }
 
 #' Read data from XLSX in extdata
@@ -32,7 +72,7 @@ run_analysis <- function(XLSX, TAG){
 #' @export
 
 read_xlsx_glaucoma <- function(XLSX) {
-  RXLSX <- system.file(package = "cobrienGlaucoma", "extdata",
+  RXLSX <- system.file(package = "cnormileGlaucoma", "extdata",
                        XLSX)
 
   xlsx_tb <- tibble::as_tibble(openxlsx::read.xlsx(RXLSX))
@@ -65,14 +105,15 @@ msigdb_pathways_to_list <- function(msigdb_species = "Homo sapiens", msigdb_cat 
 #' @param tb tibble of XLSX results
 #' @param padj numeric value for padj below which we return results
 #' @param TAG character to tag output and name plots etc
+#' @param OUTDIR character where output is saved
 #' @return NULL, writes XLSX containing pathways as sheets, and genes in those significant pathway
 #' @export
 
-msigdb_pway_xlsx <- function(fgseaRes, tb, padj, TAG){
+msigdb_pway_xlsx <- function(fgseaRes, tb, padj, TAG, OUTDIR){
   wb <- openxlsx::createWorkbook(TAG)
   olist <- lapply(seq_along(fgseaRes$padj), function(f){
     if(fgseaRes$padj[f] < padj){
-      pway_tb <- dplyr::filter(.data = tb, X1 %in% unlist(fgseaRes[f,]$leadingEdge))
+      pway_tb <- dplyr::filter(.data = tb, gene %in% unlist(fgseaRes[f,]$leadingEdge))
       ll <- strsplit(gsub("HALLMARK_", "", unlist(fgseaRes[f,"pathway"])), "")[[1]]
       if(length(ll) > 31){
         name <- paste(ll[1:31], collapse = "")
@@ -81,9 +122,35 @@ msigdb_pway_xlsx <- function(fgseaRes, tb, padj, TAG){
       }
       openxlsx::addWorksheet(wb, sheetName = name)
       openxlsx::writeData(wb = wb, sheet = name, x = pway_tb)
+      dir.create(paste0(OUTDIR, "/xlsx/"), recursive = TRUE, showWarnings = FALSE)
       openxlsx::saveWorkbook(wb = wb,
-                             file = paste0(TAG, ".sig_", padj, ".fgsea_pathway_genes.xlsx"),
+                             file = paste0(OUTDIR, "/xlsx/", TAG, ".sig_", padj, ".fgsea_pathway_genes.xlsx"),
                              overwrite = TRUE)
     }
   })
 }
+
+#' msigdb pathways genes to XLSX
+#'
+#' @param TAG character to tag output
+#' @return NULL, writes XLSX containing pathways as sheets, and genes in those significant pathway
+#' @export
+
+write_all_hallmarks <- function(TAG = "All_Hallmark_pathways"){
+  msigdb_pathlist <- msigdb_pathways_to_list()
+  wb <- openxlsx::createWorkbook(TAG)
+  lapply(names(msigdb_pathlist), function(name){
+    ssn <- strsplit(name, "")[[1]]
+    if(length(ssn) > 30){
+      sname <- paste(strsplit(name, "")[[1]][1:30], collapse = "")
+    } else {
+      sname <- name
+    }
+    openxlsx::addWorksheet(wb, sheetName = sname)
+    openxlsx::writeData(wb = wb, sheet = sname, x = msigdb_pathlist[[name]])
+    openxlsx::saveWorkbook(wb = wb,
+                           file = paste0(TAG, ".xlsx"),
+                           overwrite = TRUE)
+  })
+}
+
--- R/run_analyses.R
+++ R/run_analyses.R
@@ -5,12 +5,22 @@
 #' @export
 
 run_analyses <- function(){
+  print("Running analyses...")
+  OUTDIR <- "~/Downloads/cnormileGlaucoma"
+  dir.create(OUTDIR, showWarnings = FALSE, recursive = TRUE)
   run_analysis(XLSX = "all_glaucoma_vs_all_control.xlsx",
-               TAG = "All_Glaucoma_vs_All_Control")
+               TAG = "All_Glaucoma_vs_All_Control",
+               OUTDIR = OUTDIR)
   run_analysis(XLSX = "glaucoma_equal_sample_vs_all_control.xlsx",
-              TAG = "Equal_Glaucoma_vs_All_Control")
+              TAG = "Equal_Glaucoma_vs_All_Control",
+              OUTDIR = OUTDIR)
   run_analysis(XLSX = "dividing_MSC_run.xlsx",
-               TAG = "Dividing_MSC_Run")
+               TAG = "Dividing_MSC_Run",
+               OUTDIR = OUTDIR)
   run_analysis(XLSX = "group4_COMBINED.xlsx",
-               TAG = "Group4_Combined")
+               TAG = "Group4_Combined",
+               OUTDIR = OUTDIR)
+  run_group_analysis(XLSX = "group_markers_2024.xlsx",
+               TAG = "Group_Markers",
+               OUTDIR = OUTDIR)
 }
